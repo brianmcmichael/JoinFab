eacbeab90d4720788978de79b6f0c5d99c145a2d
Brian McMichael <brian@brianmcmichael.com>
2021-09-08 14:37:15 -0400

    Add imports

--- src/JoinFab.sol
+++ src/JoinFab.sol
@@ -1,6 +1,20 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity >=0.5.12;
 
-contract JoinFab {
+import {GemJoin}        from "dss/join.sol";
+import {DaiJoin}        from "dss/join.sol";
+import {GemJoin2}       from "dss-gem-joins/join-2.sol";
+import {GemJoin3}       from "dss-gem-joins/join-3.sol";
+import {GemJoin4}       from "dss-gem-joins/join-4.sol";
+import {GemJoin5}       from "dss-gem-joins/join-5.sol";
+import {GemJoin6}       from "dss-gem-joins/join-6.sol";
+import {GemJoin7}       from "dss-gem-joins/join-7.sol";
+import {GemJoin8}       from "dss-gem-joins/join-8.sol";
+import {AuthGemJoin}    from "dss-gem-joins/join-auth.sol";
+import {ManagedGemJoin} from "dss-gem-joins/join-managed.sol";
+
+contract GemJoinFab {
+
+
 
 }
